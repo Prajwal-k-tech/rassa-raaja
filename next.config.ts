0b4a79ab4648bb709416e360258251e0485c94d5
Prajwal K <prajwal.k141205@gmail.com>
2025-12-12 23:00:43 +0530

    Fix: Add assetPrefix to ensure image loading on GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/rassa-raaja" : "",
+  assetPrefix: isProd ? "/rassa-raaja/" : "",
   images: {
     unoptimized: true,
   },
